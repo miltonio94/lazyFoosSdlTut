4acbd0ac19d727f6cb4989df79a460cce5bd9729
Milton <miltonio94@gmail.com>
2022-06-21 09:09:19 +0100

    Finished tut 12

---
+++ 12_ColourModulation/LTexture.h
@@ -0,0 +1,26 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_rect.h>
+#include <SDL2/SDL_stdinc.h>
+#include <string>
+
+class LTexture {
+public:
+  LTexture();
+  ~LTexture();
+
+  bool loadFromFile(std::string path, SDL_Renderer *renderer);
+
+  void freeTexture();
+  void setColor(Uint8 red, Uint8 green, Uint8 blue);
+  void render(int x, int y, SDL_Renderer *renderer, SDL_Rect *clip);
+  void render(int x, int y, SDL_Renderer *renderer);
+
+  int getWidth();
+  int getHeight();
+
+private:
+  SDL_Texture *mTexture;
+
+  int mWidth;
+  int mHeight;
+};
---
+++ 12_ColourModulation/main.cpp
@@ -0,0 +1,142 @@
+#include "LTexture.h"
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_events.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_render.h>
+#include <SDL2/SDL_stdinc.h>
+#include <cstdio>
+#include <string>
+
+bool init();
+bool loadMedia();
+void close();
+
+SDL_Window *gWindow = NULL;
+SDL_Surface *gScreenSurface = NULL;
+SDL_Renderer *gRenderer = NULL;
+
+LTexture gModulatedTexture;
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+int main(int argc, char *argv[]) {
+  // init SDL
+  if (!init()) {
+    printf("Failed to initialize");
+  } else {
+    if (!loadMedia()) {
+      printf("Failed to load meadia");
+    } else {
+      bool quit = false;
+      SDL_Event e;
+
+      Uint8 r = 255;
+      Uint8 g = 255;
+      Uint8 b = 255;
+
+      while (!quit) {
+        while (SDL_PollEvent(&e) != 0) {
+          if (e.type == SDL_QUIT) {
+            quit = true;
+          } else if (e.type == SDL_KEYDOWN) {
+            switch (e.key.keysym.sym) {
+
+            case SDLK_q:
+              r += 32;
+              break;
+
+            case SDLK_w:
+              g += 32;
+              break;
+
+            case SDLK_e:
+              b += 32;
+              break;
+
+            case SDLK_a:
+              r -= 32;
+              break;
+
+            case SDLK_s:
+              g -= 32;
+              break;
+
+            case SDLK_d:
+              b -= 32;
+              break;
+            }
+          }
+        }
+
+        SDL_SetRenderDrawColor(gRenderer, 0xff, 0xff, 0xff, 0xff);
+        SDL_RenderClear(gRenderer);
+
+        gModulatedTexture.setColor(r, g, b);
+        gModulatedTexture.render(0, 0, gRenderer);
+
+        SDL_RenderPresent(gRenderer);
+      }
+    }
+  }
+  close();
+  return 0;
+}
+
+bool init() {
+  bool success = true;
+
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    printf("SDL could not initialize SDL_Error: %s\n", SDL_GetError());
+    success = false;
+  } else {
+    gWindow = SDL_CreateWindow("SDL Tuturial", SDL_WINDOWPOS_UNDEFINED,
+                               SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
+                               SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (gWindow == NULL) {
+      printf("SDL could not creat a window SDL_Error: %s\n", SDL_GetError());
+      success = false;
+    } else {
+      gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
+      if (gRenderer == NULL) {
+        printf("Couldn't create  renderer! Err: %s \n", SDL_GetError());
+        success = false;
+      } else {
+        SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+
+        int imgFlags = IMG_INIT_PNG;
+        if (!(IMG_Init(imgFlags) & imgFlags)) {
+          printf("could not initializy SDL_image! Err: %s\n", IMG_GetError());
+          success = false;
+        }
+      }
+      gScreenSurface = SDL_GetWindowSurface(gWindow);
+    }
+  }
+
+  return success;
+}
+
+bool loadMedia() {
+  bool success = true;
+
+  if (!gModulatedTexture.loadFromFile("img/colors.png", gRenderer)) {
+    printf("failed to load dots texture img \n");
+    success = false;
+  }
+
+  return success;
+}
+
+void close() {
+  gModulatedTexture.freeTexture();
+
+  SDL_DestroyRenderer(gRenderer);
+  SDL_DestroyWindow(gWindow);
+
+  gRenderer = NULL;
+  gWindow = NULL;
+
+  IMG_Quit();
+  SDL_Quit();
+}
